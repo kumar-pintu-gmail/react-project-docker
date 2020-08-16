fc19ed69bd43c1065bcab36031fb68b520d86bdb
Pintu Kumar <kumar.pintu@gmail.c                                                                                                             om>
2020-08-16 16:27:26 +0000

    travis org

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload by Pintu test
+          Edit <code>src/App.js</code> and save to reload by Pintu travisorg
         </p>
         <a
           className="App-link"
