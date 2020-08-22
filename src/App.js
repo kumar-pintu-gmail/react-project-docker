59f2753161defdc77caee2a8ba4c8b3178af0a7e
Pintu Kumar <kumar.pintu@gmail.c                                                                                                             om>
2020-08-22 16:45:25 +0000

    added 22aug5

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload by Pintu Test 22Aug4
+          Edit <code>src/App.js</code> and save to reload by Pintu Test 22Aug5
         </p>
         <a
           className="App-link"
